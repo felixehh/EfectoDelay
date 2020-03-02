79502b59b59ceba3bbef3d4e85f5c2d69d0e9f12
alumno <alumno@E108>
2020-03-02 12:49:25 -0700

    avance ganancia

--- Reproductor/EfectoDelay.cs
+++ Reproductor/EfectoDelay.cs
@@ -12,19 +12,43 @@ namespace Reproductor
     {
         private ISampleProvider fuente;
 
-        public int offsetMs;
+        private int offsetMs;
+        public int OffsetMs
+        {
+            get
+            {
+                return offsetMs;
+            }
+            set
+            {
+                if (value > 20000)
+                {
+                    offsetMs = 20000;
+                }
+                else if (value < 0)
+                {
+                    offsetMs = 0;
+                }
+                else
+                {
+                    offsetMs = value;
+                }
+            }
+        }
         private List<float> muestras = new List<float>();
         private int tamañoBuffer;
 
         private int cantidadMuestrasTranscurridas = 0;
         private int cantidadMuestrasBorradas = 0;
 
-        public EfectoDelay(ISampleProvider fuente, int offsetMs)
+
+        public EfectoDelay(ISampleProvider fuente, int offsetMiliSegundos)
         {
             this.fuente = fuente;
-            this.offsetMs = offsetMs;
+            this.offsetMs = offsetMiliSegundos;
 
-            tamañoBuffer = fuente.WaveFormat.SampleRate * 20 * fuente.WaveFormat.Channels; 
+            tamañoBuffer = fuente.WaveFormat.SampleRate * 20 *
+                fuente.WaveFormat.Channels;
         }
 
         public WaveFormat WaveFormat
@@ -38,35 +62,47 @@ namespace Reproductor
         public int Read(float[] buffer, int offset, int count)
         {
             int read = fuente.Read(buffer, offset, count);
-            //calculo de tiempo
-            float milisegundosTranscurridos = ((float)(cantidadMuestrasTranscurridas) / (float)(fuente.WaveFormat.SampleRate * fuente.WaveFormat.Channels)) * 1000.0f;
-            int numeroMuestrasOffset = (int) ((offsetMs / 1000.0f) * (float) fuente.WaveFormat.SampleRate);
+            //Cálculo de tiempos
+            float milisegundosTranscurridos =
+                ((float)(cantidadMuestrasTranscurridas) /
+                (float)(fuente.WaveFormat.SampleRate *
+                    fuente.WaveFormat.Channels)) *
+                1000.0f;
+            int numeroMuestrasOffset =
+                (int)
+                    ((offsetMs / 1000.0f)
+                    * (float)fuente.WaveFormat.SampleRate);
 
-            //llenar el buffer
-            for (int i=0; i<read; i++)
+            //Llenar el buffer
+            for (int i = 0; i < read; i++)
             {
                 muestras.Add(buffer[i + offset]);
             }
-
-            //si el buffer excede el tamaño, ajustar el num de elementos
-            if(muestras.Count > tamañoBuffer)
+            //Si el buffer excede el tamaño, ajustar el
+            //número de elementos
+            if (muestras.Count > tamañoBuffer)
             {
-                //eliminar excedente
-                int diferencia = muestras.Count - tamañoBuffer;
+                //Eliminar excedente
+                int diferencia =
+                    muestras.Count - tamañoBuffer;
                 muestras.RemoveRange(0, diferencia);
                 cantidadMuestrasBorradas += diferencia;
             }
-            //aplicar el efecto
-            if (milisegundosTranscurridos > offsetMs)
+            //Aplicar el efecto
+            if (milisegundosTranscurridos >
+                offsetMs)
             {
-                for(int i=0; i<read; i++)
+                for (int i = 0; i < read; i++)
                 {
-                    buffer[i + offset] += muestras[(cantidadMuestrasTranscurridas - cantidadMuestrasBorradas) + i];
+                    buffer[i + offset] +=
+                        muestras[
+                            (cantidadMuestrasTranscurridas -
+                            cantidadMuestrasBorradas) + i
+                            - numeroMuestrasOffset];
                 }
             }
 
             cantidadMuestrasTranscurridas += read;
-
             return read;
         }
     }
